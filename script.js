7c834fb21873ff37461199c46dddb3be35522e01
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-11-10 10:42:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "65%";
+    document.getElementById("body").style.cssText = "background-color: rgba(0,0,0, 0.3);";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("body").style.cssText = "background-color: rgba(255,255,255);";
+}
